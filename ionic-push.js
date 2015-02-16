f3835c68609dee3fa565cbeb6d4828fd140f1696
Rudy <drawncloser@gmail.com>
2015-02-16 12:04:00 -0600

    Updated to use register() function, rather than triggering on device ready.

--- ionic-push.js
+++ ionic-push.js
@@ -2,69 +2,67 @@ angular.module('ionic.service.push', ['ngCordova', 'ionic.service.core'])
 
 /**
  * The Ionic Push service client wrapper.
- * 
+ *
  * Example:
- * 
+ *
  * angular.controller(['$scope', '$ionicPush', function($scope, $ionicPush) {
- *  
- *   
- }])
+ * }])
  *
  */
-.factory('$ionicPush', [
-  '$http', '$cordovaPush', '$ionicApp', '$log',
-  
-function($http, $cordovaPush, $ionicApp, $log) {
+    .factory('$ionicPush', [
+        '$http', '$cordovaPush', '$ionicApp', '$log',
 
-  var iosConfig = {
-    "badge": true,
-    "sound": true,
-    "alert": true,
-  };
+        function($http, $cordovaPush, $ionicApp, $log) {
 
-  // Grab the current app
-  var app = $ionicApp.getApp();
-  
-  if(!app || !app.app_id) {
-    $log.error('PUSH: Unable to initialize, you must call $ionicAppProvider.identify() first');
-  }
+            //Default configuration for iOS
+            var iosConfig = {
+                "badge": true,
+                "sound": true,
+                "alert": true
+            };
 
-  function init() {
-    var api = $ionicApp.getValue('push_api_server');
-    $log.debug('PUSH: Connecting to push api', api);
+            // Grab the current app
+            var app = $ionicApp.getApp();
 
-    $cordovaPush.register(iosConfig).then(function(result) {
+            //Check for required credentials
+            if(!app || !app.app_id) {
+                $log.error('PUSH: Unable to initialize, you must call $ionicAppProvider.identify() first');
+            }
 
-      // Success -- send deviceToken to server, and store 
-      var req = {
-        method: 'POST',
-        url: api + "/api/v1/register-device-token",
-        headers: {
-          'X-Ionic-Applicaton-Id': $ionicApp.getId(),
-          'X-Ionic-API-Key': $ionicApp.getApiKey()
-        },
-        data: {
-          ios_token: token,
-          metadata: {
-          }
-        }
-      };
+            function init(metadata) {
+                var api = $ionicApp.getValue('push_api_server');
+                $log.debug('PUSH: Connecting to push api');
 
-      $http(req)
-        .success(function(data, status) {
-          alert("Success: " + data);
-        })
-        .error(function(error, status, headers, config) {
-          alert("Error: " + error + " " + status + " " + headers);
-        });
-    });
-  }
+                $cordovaPush.register(iosConfig).then(function(token) {
+                    $log.debug('Success! Registering token with Ionic');
 
-  document.addEventListener("deviceready", function() {
-    // Wait until the device is ready
-    app && init();
-  });
+                    // Success -- send deviceToken to server, and store
+                    var req = {
+                        method: 'POST',
+                        url: api + "/api/v1/register-device-token",
+                        headers: {
+                            'X-Ionic-Application-Id': $ionicApp.getId(),
+                            'X-Ionic-API-Key': $ionicApp.getApiKey()
+                        },
+                        data: {
+                            ios_token: token,
+                            metadata: metadata
+                        }
+                    };
 
-  return {
-  }
-}]);
+                    $http(req)
+                        .success(function(data, status) {
+                            $log.debug("Success: " + data);
+                        })
+                        .error(function(error, status, headers, config) {
+                            $log.debug("Error: " + error + " " + status + " " + headers);
+                        });
+                });
+            }
+
+            return {
+                register: function(metadata){
+                    app && init(metadata);
+                }
+            }
+        }]);
