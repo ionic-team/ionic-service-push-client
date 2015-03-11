616999a60a365713918a2c4c99fe82ca4b4dac10
Max Lynch <max@drifty.com>
2015-03-11 14:02:27 -0500

    fix(android): pushUnique

--- ionic-push.js
+++ ionic-push.js
@@ -145,7 +145,7 @@ function($http, $cordovaPush, $ionicApp, $ionicUser, $rootScope, $log, $q) {
 
     // Push the token into the user data
     try {
-      $ionicUser.push('_push.android_tokens', token);
+      $ionicUser.push('_push.android_tokens', token, true);
     } catch(e) {
       console.warn('Received push token before user was identified and will not be synced with ionic.io. Make sure to call $ionicUser.identify() before calling $ionicPush.register.');
     }
