1ff2e6257c9b0c2e8761aed40bbdffa0f1eacf40
William Pelrine <drawncloser@gmail.com>
2015-03-19 14:09:43 -0500

    Update ionic-push.js

--- ionic-push.js
+++ ionic-push.js
@@ -53,7 +53,7 @@ function($http, $cordovaPush, $ionicApp, $ionicPushActions, $ionicUser, $rootSco
 
         // Push the token into the user data
         try {
-          $ionicUser.push('_push.ios_tokens', token, true);
+          $ionicUser.push('_push.ios_tokens', [token], true);
         } catch(e) {
           console.warn('Received push token before user was identified and will not be synced with ionic.io. Make sure to call $ionicUser.identify() before calling $ionicPush.register.');
         }
@@ -128,7 +128,7 @@ function($http, $cordovaPush, $ionicApp, $ionicPushActions, $ionicUser, $rootSco
   function androidInit(token, metadata) {
     // Push the token into the user data
     try {
-      $ionicUser.push('_push.android_tokens', token, true);
+      $ionicUser.push('_push.android_tokens', [token], true);
     } catch(e) {
       console.warn('Received push token before user was identified and will not be synced with ionic.io. Make sure to call $ionicUser.identify() before calling $ionicPush.register.');
     }
